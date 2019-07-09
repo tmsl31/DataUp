eed2e8e05eb840a02a5fd710af13a5bf7268bfa6
Tomás Lara <tmslara.a@gmail.com>
2019-07-09 15:32:52 -0400

    Update interfaz.py

--- interfaz.py
+++ interfaz.py
@@ -6,77 +6,114 @@ from DataUp import *
 # Tkinter.
 from tkinter import *
 # Grafico.
-import matplotlib as mpl
 import numpy as np
-import sys
-if sys.version_info[0] < 3:
-    import Tkinter as tk
-else:
-    import tkinter as tk
-import matplotlib.backends.tkagg as tkagg
-from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import NavigationToolbar2Tk
 
-#Definion de la interfaz.
-root = Tk()
-#Cambiar color de fondo
-root.config(bg="lightblue") 
-root.wm_title("DataUp")
+class SampleApp(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+        self._frame = None
+        self.switch_frame(StartPage)
 
-#Valor del tiempo de grabacion (default =30)
-tiempoGrab = 30;
-#Caracteristicas.
-caracteristicas = ['dulce','amargo','burbuja'];
-#Calificaciones.
-calificaciones = [0,1,2];
-fig_photo = 0;
-
-#Definicion de funciones a utilizar en la interfaz.
-def obtenerTiempo(tiempoGrab):
-    #Funcion que obtenga el tiempo de grabacion.
-        #Definicion de los metodos de la clase.
-        tiempoGrab = int(tiempoIngresar.get())
-        print(tiempoGrab)
+    def switch_frame(self, frame_class):
+        new_frame = frame_class(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new_frame
+        self._frame.pack()
         
-def grabacion(tiempoGrab):
-    #Funcion que realiza la grabacion.
-    #Obtener el valor del tiempo de grabacion.
-    #Llamar a visualizar
-    caracteristicas, calificaciones = procesarAudio(tiempoEspera = tiempoGrab)
-    
+    #Definicion de funciones a utilizar en la interfaz.
+    def obtenerTiempo(tiempoGrab):
+        #Funcion que obtenga el tiempo de grabacion.
+            #Definicion de los metodos de la clase.
+            tiempoGrab = int(tiempoIngresar.get())
+            print(tiempoGrab)
 
-def displayGrafico(caracteristicas,calificaciones):
-    #Display del grafico en la interfaz.
-    # Generar la figura.
-    figura = visualizacion2(caracteristicas,calificaciones)
-    #
-    # Keep this handle alive, or else figure will disappear
-    fig_x, fig_y = 100, 100
-    fig_photo = draw_figure(canvas, figura, loc=(fig_x, fig_y))
-    fig_w, fig_h = fig_photo.width(), fig_photo.height()
+    def grabacion(tiempoGrab):
+        #Funcion que realiza la grabacion.
+        #Obtener el valor del tiempo de grabacion.
+        #Llamar a visualizar
+        try:
+            caracteristicas, calificaciones = procesarAudio(tiempoEspera = tiempoGrab)
+        except:
+            print('Error en la grabacion. Reintentar')
+        #Cambiar a la pagina del grafico.
+        master.switch_frame(PlotPage)
+            
+    def matplotCanvas(caracteristicas,calificaciones):
+        #Display del grafico en la interfaz.
+        #Transicion a la pagina del grafico.
+        
+        #Numero de caracteristicas
+        AttNo = len(caracteristicas)
+        #Creacion de la figura.
+        f = Figure(figsize=(5,5),dpi = 100)
+        ax = f.add_subplot(111, polar = True)
+        #Definicion de los angulos.
+        angles = [n / float(AttNo) * 2 * np.pi for n in range(AttNo)]
+        angles += angles [:1]
+        angles = np.array(angles)
+        #
+        calificaciones = np.append(calificaciones,calificaciones[:1])
+        #Agregar los atributos al grafico.
+        #Agregar valor al atributo.
+        car = agregarValor(caracteristicas,calificaciones)
+        #Angulos
+        #plt.xticks(angles,car)  
+        ax.set_thetagrids(angles * 180.0/np.pi, car)
+        #
+        ax.plot(angles,calificaciones,'o-')
+        #Fill in the area plotted in the last line
+        ax.fill(angles, calificaciones, 'teal', alpha=0.1)
+        #Titulo.
+        ax.set_title("Resultados encuesta")
+        #Generar canvas
+        canvas = FigureCanvasTkAgg(f,root)
+        canvas.get_tk_widget().pack()
+
+class StartPage(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        Label(self, text="Data Up", font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
+        Button(self, text="Realizar una Grabación",command=lambda: master.switch_frame(RecordPage)).pack()
+        Button(self, text="Ver Datos registrados",command=lambda: master.switch_frame(DataPage)).pack()
+        
+class RecordPage(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        Frame.configure(self,bg='lightblue')
+        Label(self, text="Data Up", font=('Helvetica', 18, "bold")).pack(side=TOP, expand = True, fill=X)
+        Button(self, text="Volver principal", command=lambda: master.switch_frame(StartPage)).pack(side="top", fill="x", pady=5)
+        #Etiqueta de ingreso de datos.
+        self.etiquetaIngreso = Label(self, text='Tiempo de grabación').pack(side=TOP)
+        #Ventana de ingreso de texto
+        self.tiempoIngresar = Entry(self).pack(side=TOP)
+        #Boton de set tiempo.
+        self.botonSet = Button(self, text='Set', command =lambda: master.obtenerTiempo(tiempoGrab)).pack(side=TOP)
+        #Boton de grabar.
+        self.fotoMic= PhotoImage(file=r"C:\Users\tlara\OneDrive\Documentos\GitHub\DataUp\microphone.png")
+        self.fotoMic2 = self.fotoMic.subsample(5,5)
+        self.botonGrabar = Button(self, text = 'Grabar!', image = self.fotoMic2, compound = LEFT, command =lambda: master.grabacion(tiempoGrab)).pack(side=BOTTOM, fill="x", pady=5)
 
 
-#Interfaz.
-#Etiqueta de ingreso de datos.
-etiquetaIngreso = Label(root, text='Tiempo de grabación')
-etiquetaIngreso.place(x=50, y=50)
-#Ventana de ingreso de texto
-tiempoIngresar = Entry()
-tiempoIngresar.place(x=170, y=50)
-#Boton de set tiempo.
-botonSet = Button(root, text='Set', command =lambda: obtenerTiempo(tiempoGrab))
-botonSet.place(x = 300, y = 48)
-#Boton de grabar.
-fotoMic= PhotoImage(file=r"C:\Users\tlara\OneDrive\Documentos\GitHub\DataUp\microphone.png")
-fotoMic2 = fotoMic.subsample(5,5)
-botonGrabar = Button(root, text = 'Grabar!', image = fotoMic2, compound = LEFT, command =lambda: grabacion(tiempoGrab))
-botonGrabar.place(x=160,y=100)
-#Creacion del canvas.
-canvas = tk.Canvas(root, width=200, height=200)
-canvas.pack()
-#Boton de display del grafico.
-botonGrafico = Button(root, text = 'Mostrar Grafico', compound = LEFT, command =lambda: displayGrafico(caracteristicas,calificaciones))
-botonGrafico.place(x=160,y=200)
-
-
-#Correr la interfaz
-root.mainloop()
+class PlotPage(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        Frame.configure(self,bg='red')
+        Label(self, text="Resultado encuesta", font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
+        Button(self, text="Volver principal",command=lambda: master.switch_frame(StartPage)).pack(side=TOP)
+                #Boton de display del grafico.
+        self.botonGrafico = Button(self, text = 'Mostrar Grafico', compound = LEFT, command =lambda: master.matplotCanvas(caracteristicas,calificaciones)).pack(side=TOP)
+        
+class DataPage(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        Frame.configure(self,bg='red')
+        Label(self, text="Datos recopilados", font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
+        Button(self, text="Volver principal",command=lambda: master.switch_frame(StartPage)).pack()            
+        
+if __name__ == "__main__":
+    app = SampleApp()
+    app.mainloop()   
