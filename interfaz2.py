59a7c97060c48939b8542039f9c82a18e1e62327
Tomás Lara <tmslara.a@gmail.com>
2019-07-08 20:43:04 -0400

    interfaz

--- interfaz2.py
+++ interfaz2.py
@@ -1,27 +1,71 @@
 #nterfaz del proyecto DataUp.
 
-#Imports.
+# Imports.
+# Modulo de DataUp
 from DataUp import *
-from tkinter import Tk, Label, Button
+# Tkinter.
 from tkinter import *
-from tkinter.ttk import *
+# Grafico.
+import matplotlib as mpl
+import numpy as np
+import sys
+if sys.version_info[0] < 3:
+    import Tkinter as tk
+else:
+    import tkinter as tk
+import matplotlib.backends.tkagg as tkagg
+from matplotlib.backends.backend_agg import FigureCanvasAgg
 
-#Generar ventana de tkinter.
-root = Tk() 
+#Define la interfaz como una clase.
+class ventanaDataUp:
+    def __init__(self, win):
+        #Etiqueta de ingreso de datos.
+        self.etiquetaIngreso = Label(win, text='Tiempo de grabación')
+        self.etiquetaIngreso.place(x=50, y=50)
+        #Ventana de ingreso de texto
+        self.tiempoIngresar = Entry()
+        self.tiempoIngresar.place(x=170, y=50)
+        #Boton de set tiempo.
+        self.botonSet = Button(win, text='Set')
+        self.botonSet.place(x = 300, y = 48)
+        #Boton de grabar.
+        self.fotoMic= PhotoImage(file=r"C:\Users\tlara\OneDrive\Documentos\GitHub\DataUp\microphone.png")
+        self.fotoMic2 = self.fotoMic.subsample(5,5)
+        self.botonGrabar = Button(win, text = 'Grabar!', image = self.fotoMic2, compound = LEFT, command = self.grabacion)
+        self.botonGrabar.place(x=160,y=100)
+        win.config(bg="gray") #Cambiar color de fondo
+        
+    #Definicion de los metodos de la clase.
+    def grabacion(self):
+        #Obtener el valor del tiempo de grabacion.
+        tiempoGrabacion = int(self.tiempoIngresar.get())
+        #Llamar a visualizar
+        obtenerGrafico(tiempoEspera = tiempoGrabacion)
+        
+    def displayResultados(self):
+        print('Obteniendo grafico')
+        
+    def draw_figure(canvas, figure, loc=(50, 300)):
+        figure_canvas_agg = FigureCanvasAgg(figure)
+        figure_canvas_agg.draw()
+        figure_x, figure_y, figure_w, figure_h = figure.bbox.bounds
+        figure_w, figure_h = int(figure_w), int(figure_h)
+        photo = tk.PhotoImage(master=canvas, width=figure_w, height=figure_h)
 
-# Adding widgets to the root window 
-Label(root, text = 'DataUp', font =('Verdana', 15)).pack(side = TOP, pady = 10) 
+        # Position: convert from top-left anchor to center anchor
+        canvas.create_image(loc[0] + figure_w/2, loc[1] + figure_h/2, image=photo)
 
-# Creating a photoimage object to use image 
-photo = PhotoImage(file = r"C:\Users\tlara\OneDrive\Documentos\GitHub\DataUp\microphone.png") 
+        # Unfortunately, there's no accessor for the pointer to the native renderer
+        tkagg.blit(photo, figure_canvas_agg.get_renderer()._renderer, colormode=2)
 
-# Resizing image to fit on button 
-photoimage = photo.subsample(5, 5) 
+        # Return a handle which contains a reference to the photo object
+        # which must be kept live or else the picture disappears
+        return photo
+    
 
-# here, image option is used to 
-# set image on button 
-# compound option is used to align 
-# image on LEFT side of button 
-Button(root, text = 'Grabar!', image = photoimage, compound = LEFT).pack(side = TOP) 
-
-mainloop() 
+        
+window=Tk()
+mywin=ventanaDataUp(window)
+window.title('DataUp')
+window.geometry("400x300+10+10")
+window.mainloop()
